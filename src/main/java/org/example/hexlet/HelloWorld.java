1b1ceabb8351e6d8c2ae486fd41f6a0b894c4e8d
e4riya <kotkovdima0@gmail.com>
2025-06-29 19:30:25 +0300

    add jte

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,15 +1,27 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import org.example.hexlet.Courses.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class HelloWorld {
+    public static List<Course> courses = new ArrayList<>(List.of(new Course("Java", "Java course"),
+                                                                 new Course("PHP", "PHP course"),
+                                                                 new Course("Python", "Python course")));
+
     public static void main(String[] args) {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
         });
-        app.get("/hello",ctx -> {
-            String name = ctx.queryParamAsClass("name",String.class).getOrDefault("World");
-            ctx.result("Hello, " + name + "!");
+        app.get("/cources",ctx -> {
+            var header = "Курсы по программированию";
+            ctx.render("courses/index.jte", model("page", courses,"header", header));
         });
         app.start(7070);
     }
